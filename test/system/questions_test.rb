337e52c401016c7ac0ef49f2b381c49aa437ae43
JeanGuillaume Vicente <jg.vicente@wanadoo.fr>
2022-11-14 12:01:12 +0100

    tests again

--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -1,17 +1,17 @@
 require 'application_system_test_case'
 
 class QuestionsTest < ApplicationSystemTestCase
-  # test 'visiting /ask renders the form' do
-  #   visit(ask_url)
-  #   assert_selector('p', text: 'Ask your coach anything')
-  # end
-
-  test 'saying Hello yields a grumpy response from the coach' do
-    visit ask_url
-    fill_in 'question', with: 'Hello'
-    click_on 'Ask'
-    assert_text "I don't care, get dressed and go to work!"
+  test 'visiting /ask renders the form' do
+    visit(ask_url)
+    assert_selector('p', text: 'Ask your coach anything')
   end
+
+  # test 'saying Hello yields a grumpy response from the coach' do
+  #   visit ask_url
+  #   fill_in 'question', with: 'Hello'
+  #   click_on 'Ask'
+  #   assert_text "I don't care, get dressed and go to work!"
+  # end
 
   # test 'asking a question a question to the coach yields a grumpy comment' do
   #   visit ask_url
